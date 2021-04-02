827ec481c7138aa4489808ce397069f105f7200c
Luka <krivacevicluka@gmail.com>
2021-04-02 19:32:25 +0200

    RegisterFragment & EventObserver

--- app/src/main/java/com/chelios/lukabook/other/Event.kt
+++ app/src/main/java/com/chelios/lukabook/other/Event.kt
@@ -1,5 +1,7 @@
 package com.chelios.lukabook.other
 
+import androidx.lifecycle.Observer
+
 //for: in error case, after rotating device: snackbars not to show twice
 class Event<out T>(private val content: T) {
 
@@ -16,5 +18,30 @@ class Event<out T>(private val content: T) {
 
     //if event is already handled or just want to get the content without consuming it
     fun peekContent() = content
+}
 
+class EventObserver<T>(
+    private inline val onError: ((String) -> Unit)? = null,
+    private inline val onLoading: (() -> Unit)? = null,
+    private inline val onSuccess: (T) -> Unit
+) : Observer<Event<Resource<T>>> {
+    override fun onChanged(t: Event<Resource<T>>?) {
+        when(val content = t?.peekContent()) {
+            is Resource.Success -> {
+                content.data?.let(onSuccess)
+            }
+            is Resource.Error -> {
+                t.getContentIfNotHandled()?.let {
+                    onError?.let { error ->
+                        error(it.message!!)
+                    }
+                }
+            }
+            is Resource.Loading -> {
+                onLoading?.let { loading ->
+                    loading()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/chelios/lukabook/ui/auth/fragments/RegisterFragment.kt
+++ app/src/main/java/com/chelios/lukabook/ui/auth/fragments/RegisterFragment.kt
@@ -2,15 +2,36 @@ package com.chelios.lukabook.ui.auth.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.chelios.lukabook.R
+import com.chelios.lukabook.other.EventObserver
+import com.chelios.lukabook.ui.auth.AuthViewModel
+import com.chelios.lukabook.ui.snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_register.*
 
+@AndroidEntryPoint
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var viewModel: AuthViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(AuthViewModel::class.java) //bind viewModel to lifecycle of activity
+        subscribeToObservers()
+
+        btnRegister.setOnClickListener {
+            viewModel.register(
+                etEmail.text.toString(),
+                etUsername.text.toString(),
+                etPassword.text.toString(),
+                etRepeatPassword.text.toString()
+            )
+        }
 
         tvLogin.setOnClickListener {
             if(findNavController().previousBackStackEntry != null) {
@@ -22,6 +43,19 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
             }
 
         }
+    }
+
+    fun subscribeToObservers() {
+        viewModel.registerStatus.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                registerProgressBar.isVisible = false
+                snackbar(it)
+            },
+            onLoading = { registerProgressBar.isVisible = true }
+        ) {
+            registerProgressBar.isVisible = false
+            snackbar(getString(R.string.success_registration))
+        })
     }
 
 }
