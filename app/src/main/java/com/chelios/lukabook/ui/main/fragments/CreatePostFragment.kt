a926e8843e3f296e8de4d3113d9c76de4c8b4282
Luka <krivacevicluka@gmail.com>
2021-04-04 15:40:04 +0200

    Slide up Views animation

--- app/src/main/java/com/chelios/lukabook/ui/main/fragments/CreatePostFragment.kt
+++ app/src/main/java/com/chelios/lukabook/ui/main/fragments/CreatePostFragment.kt
@@ -16,6 +16,7 @@ import com.bumptech.glide.RequestManager
 import com.chelios.lukabook.R
 import com.chelios.lukabook.other.EventObserver
 import com.chelios.lukabook.ui.main.viewmodels.CreatePostViewModel
+import com.chelios.lukabook.ui.slideUpViews
 import com.chelios.lukabook.ui.snackbar
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
@@ -71,6 +72,8 @@ class CreatePostFragment : Fragment(R.layout.fragment_create_post) {
                 viewModel.createPost(uri, etPostDescription.text.toString())
             } ?: snackbar(getString(R.string.error_no_image_chosen))
         }
+
+        slideUpViews(requireContext(), ivPostImage, btnSetPostImage, tilPostText, btnPost)
     }
 
     private fun subscribeToObservers() {
