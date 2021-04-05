cc7407a0e2d999cd6593853f1eea5be32581458e
Luka <krivacevicluka@gmail.com>
2021-04-05 14:29:33 +0200

    Post Interactions

--- app/src/main/java/com/chelios/lukabook/data/entities/Post.kt
+++ app/src/main/java/com/chelios/lukabook/data/entities/Post.kt
@@ -14,5 +14,5 @@ data class Post (
         val date: Long = 0L, //date of post in milliseconds (for sorting posts chronologically)
         @get:Exclude var isLiked: Boolean = false,   //if currently logged in user liked this post
         @get:Exclude var isLiking: Boolean = false,  //when user likes a post, network request is made and during that delay user can not like same post again (disable like image button during that)
-        val likedBy: List<String> = listOf() //list of user uid-s who liked the post
+        var likedBy: List<String> = listOf() //list of user uid-s who liked the post
 )
