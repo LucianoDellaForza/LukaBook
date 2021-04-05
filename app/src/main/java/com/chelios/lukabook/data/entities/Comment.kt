bd76545904c26bd89b86203a5f9239a89e865d35
Luka <krivacevicluka@gmail.com>
2021-04-05 22:50:29 +0200

    Parsing fix

--- app/src/main/java/com/chelios/lukabook/data/entities/Comment.kt
+++ app/src/main/java/com/chelios/lukabook/data/entities/Comment.kt
@@ -5,13 +5,11 @@ import com.google.firebase.firestore.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class Comment (
-        val commentId: String,
-        val postId: String, //post id that comment belongs to
-        val uid: String,    //user id who made the comment
-        @get:Exclude
-        var username: String,
-        @get:Exclude
-        var profilePictureUrl: String,
-        val comment: String,
+        val commentId: String = "",
+        val postId: String = "", //post id that comment belongs to
+        val uid: String = "",    //user id who made the comment
+        @get:Exclude var username: String = "",
+        @get:Exclude var profilePictureUrl: String = "",
+        val comment: String = "",
         val date: Long = System.currentTimeMillis()
 )
